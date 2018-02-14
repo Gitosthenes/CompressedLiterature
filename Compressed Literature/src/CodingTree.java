e08b72bd2cef60bc99bfb2165c79345cade37596
Alex Bledsoe <nketchum.github@gmail.com>
2018-02-14 01:03:26 -0800

    Works on test file. Need to test on large file and write javadocs.

---
+++ Compressed Literature/src/CodingTree.java
@@ -0,0 +1,184 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * 
+ * @author Alex Bledsoe
+ * @version Feb 13, 2018
+ */
+public class CodingTree {
+	
+	private static final int NUM_ASCII_VALUES = 256;
+	
+	/**  */
+	public Map<Character, String> codes;
+	
+	/**  */
+	public String bits;
+	
+	/**
+	 * 
+	 */
+	public CodingTree(String theMessage) {
+		//Initialize public fields.
+		codes = new HashMap<>();
+		bits = "";
+		
+		/* Get character frequencies. The index represents the character's 
+		 * ASCII value, and the value stored at that index represents the
+		 * frequency of that character in the file. */
+		int[] charFrequency = getCharFrequency(theMessage);
+		
+		/* Build a Huffman tree using the character frequencies, keeping
+		 * a reference to the root of the tree. */
+		Node root = buildHuffmanTree(charFrequency);
+		
+		//Creates a map with the binary codes for each character.
+		writeHashMap(root, "");
+		
+		//Uses the map of codes to convert the original string into a binary string.
+		writeBits(theMessage);
+	}
+
+	/**
+	 * 
+	 * 
+	 * @param theMessage
+	 * @return
+	 */
+	private int[] getCharFrequency(String theMessage) {
+		int[] charFrequency = new int[NUM_ASCII_VALUES];
+		for (int c = 0; c < theMessage.length(); c++) {
+			char thisChar = theMessage.charAt(c);
+			charFrequency[(int) thisChar]++;
+		}
+		return charFrequency;
+	}
+
+	/**
+	 * 
+	 * 
+	 * @param theCharFrequency
+	 * @return
+	 */
+	private Node buildHuffmanTree(final int[] theCharFrequency) {
+		ArrayList<Node> nodes = new ArrayList<>();
+		for (int i = 0; i < theCharFrequency.length; i++) {
+			if (theCharFrequency[i] > 0) {
+				nodes.add(new Node((char) i, theCharFrequency[i]));
+			}
+		}
+		MyPriorityQueue<Node> pq = new MyPriorityQueue<>(nodes);
+		while (pq.size() > 1) {
+			Node left = pq.remove();
+			Node right = pq.remove();
+			Node parent = new Node((char) 0, left.getCount() + right.getCount(), left, right);
+			pq.add(parent);
+		}
+		return pq.remove();
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param root
+	 */
+	private void writeHashMap(final Node theRoot, String theValue) {
+		if (!theRoot.isLeaf()) {
+			writeHashMap(theRoot.getLeft(), theValue + "0");
+			writeHashMap(theRoot.getRight(), theValue + "1");
+		} else {
+			codes.put(theRoot.getChar(), theValue);
+		}
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param theMessage
+	 */
+	private void writeBits(String theMessage) {
+		for (int i = 0; i < theMessage.length(); i++) {
+			char thisChar = theMessage.charAt(i);
+			bits += codes.get(thisChar);
+		}
+	}
+	
+	public class Node implements Comparable<Node> {
+		
+		/**  */
+		private char myChar;
+		/** */
+		private int myCount;
+		/**  */
+		private final Node myLeft;
+		/**  */
+		private final Node myRight;
+		
+		/**
+		 * 
+		 * @param theChar
+		 * @param theCount
+		 */
+		public Node(char theChar, int theCount) {
+			this(theChar, theCount, null, null);
+		}
+		
+		public Node(char theChar, int theCount, Node theLeft, Node theRight) {
+			myChar = theChar;
+			myCount = theCount;
+			myLeft = theLeft;
+			myRight = theRight;
+		}
+		
+		/**
+		 * 
+		 * @return
+		 */
+		public char getChar() {
+			return myChar;
+		}
+		
+		/**
+		 * 
+		 * @return
+		 */
+		public int getCount() {
+			return myCount;
+		}
+		
+		/**
+		 * 
+		 * @return
+		 */
+		public Node getLeft() {
+			return myLeft;
+		}
+		
+		/**
+		 * 
+		 * @return
+		 */
+		public Node getRight() {
+			return myRight;
+		}
+		
+		/**
+		 * 
+		 * @return
+		 */
+		public boolean isLeaf() {
+			return (myLeft == null && myRight == null);
+		}
+
+		/**
+		 * 
+		 */
+		@Override
+		public int compareTo(final Node theOther) {
+			return myCount - theOther.getCount();
+		}		
+	}
+}
--- Compressed Literature/src/Main.java
+++ Compressed Literature/src/Main.java
@@ -1,7 +1,14 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.Random;
 
 /**
  * Main serves as the controller that uses instances of 
@@ -12,27 +19,77 @@ import java.util.Random;
  */
 public class Main {
 
-	public static void main(String[] args) {
-		testMyPriorityQueue();
+	/**
+	 * 
+	 * 
+	 * @param theArgs
+	 * @throws IOException
+	 */
+	public static void main(final String[] theArgs) throws IOException {
+		
+		//Setup to read file.
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(new FileInputStream("WarAndPeace.txt")));
+		String message = "";
+		int c;
+		
+		//Read file and turn into single string.
+		while ((c = buffer.read()) != -1) {
+			char thisChar = (char) c;
+			message += thisChar;
+		}
+		buffer.close();
+		
+		//Pass string to instance of CodingTree to compress.
+		final CodingTree shrunk = new CodingTree(message);
+		
+		//Output codes to text file.
+		PrintWriter writer = new PrintWriter("codes.txt", "UTF-8");
+		writer.println("{");
+		for (char key : shrunk.codes.keySet()) {
+			String value = shrunk.codes.get(key);
+			writer.println(key + "=" + value + ", ");
+		}
+		writer.println("}");
+		writer.close();
+		
+		//Output compressed binary string to binary file.
+		OutputStream out = new FileOutputStream("output.txt");
+		byte[] b = new byte[(shrunk.bits.length() / 8) + 1];
+		int currentIndex = 0;
+		String currentByte = "";
+		for (int i = 0; i < shrunk.bits.length(); i++) {
+			currentByte += shrunk.bits.charAt(i);
+			if ((i + 1) % 8 == 0) {
+				b[currentIndex++] = ((byte) Integer.parseInt(currentByte, 2));
+				currentByte = "";
+			}
+		}
+		if (currentByte.length() != 0) {
+			b[currentIndex] = Byte.parseByte(currentByte);
+		}
+		out.write(b);
+		out.close();
+
+//		testMyPriorityQueue();
 	}
 	
-	/**
-	 * Tests MyPriorityQueue for binary heap structure and order properties by initializing a
-	 * random collection of letters and creating a MyPriorityQueue from it. The letters should
-	 * then be printed back out to the console in alphabetical order.
-	 */
-	private static void testMyPriorityQueue() {
-		Random rand = new Random();
-		List<Character> charList = Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
-												 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
-		ArrayList<Character> test = new ArrayList<>();
-		for (int i = 1; i < 32; i++) {
-			test.add(charList.get(rand.nextInt(charList.size())));
-		}
-		MyPriorityQueue<Character> testPQ = new MyPriorityQueue<>(test);
-
-		while (!testPQ.isEmpty()) {
-			System.out.println("Size Before Removal: " + testPQ.size() + ", Min Element: " + testPQ.remove());			
-		}
-	}
+//	/**
+//	 * Tests MyPriorityQueue for binary heap structure and order properties by initializing a
+//	 * random collection of letters and creating a MyPriorityQueue from it. The letters should
+//	 * then be printed back out to the console in alphabetical order.
+//	 */
+//	private static void testMyPriorityQueue() {
+//		Random rand = new Random();
+//		List<Character> charList = Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+//												 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+//		ArrayList<Character> test = new ArrayList<>();
+//		for (int i = 1; i < 32; i++) {
+//			test.add(charList.get(rand.nextInt(charList.size())));
+//		}
+//		MyPriorityQueue<Character> testPQ = new MyPriorityQueue<>(test);
+//
+//		while (!testPQ.isEmpty()) {
+//			System.out.println("Size Before Removal: " + testPQ.size() + ", Min Element: " + testPQ.remove());			
+//		}
+//	}
 }
